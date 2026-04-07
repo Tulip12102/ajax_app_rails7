f2d56baafa76cb0682ae7e71328f8e4086a75220
Tulip12102 <yamamotosayaka1121@gmail.com>
2026-04-07 23:57:17 +0900

    環境変数の設定

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -5,9 +5,4 @@ set -o errexit
 bundle install
 bundle exec rake assets:precompile
 bundle exec rake assets:clean
-<<<<<<< HEAD
-# bundle exec rake db:migrate
-DISABLE_DATABASE_ENVIRONMENT_CHECK=1 bundle exec rake db:migrate:reset
-=======
 bundle exec rake db:migrate
->>>>>>> a5389a08f5f6feb13c13a20b8c4969d31851ba4a
